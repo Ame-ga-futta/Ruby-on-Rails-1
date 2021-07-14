e305b2a090e95946f19490ca633ea4d686ae7f35
torimoto <torimotoame@torimotohiroshitakanoMacBook-Air.local>
2021-07-14 21:05:25 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+  def top
+  end
+  
+  def index
+    @posts = Post.all.order(start_date: "ASC")
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:title, :start_date, :end_date, :all_day, :text))
+    if @post.save
+      flash[:notice] = "スケジュールを新規登録しました"
+      redirect_to :posts
+    else
+      render "new"
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :start_date, :end_date, :all_day, :text))
+      flash[:notice] = "スケジュールを編集しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :posts
+  end
+end
